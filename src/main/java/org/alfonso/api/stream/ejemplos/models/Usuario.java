2f73a1dc24bb9339f5579c4d6b5d29c7271a3507
ladri <ladrin@gmail.com>
2025-09-10 16:56:56 +0200

    wip

---
+++ src/main/java/org/alfonso/api/stream/ejemplos/models/Usuario.java
@@ -0,0 +1,57 @@
+package org.alfonso.api.stream.ejemplos.models;
+
+import java.util.Objects;
+
+public class Usuario
+{
+    private String nombre;
+    private String apellido;
+    final private Integer edad;
+    private String dni;
+    private Integer id;
+    private static int ultimoId;
+
+    public Usuario(String nombre, String apellido,Integer edad, String dni)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni;
+        this.id = ++ultimoId;
+    }
+
+    public String getNombre() {return nombre;}
+
+    public String getApellido() {return apellido;}
+
+    public Integer getEdad() {return edad;}
+
+    public String getDni() {return dni;}
+
+    public Integer getId() {return id;}
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public void setApellido(String apellido) {this.apellido = apellido;}
+
+    public void setDni(String dni) {this.dni = dni;}
+
+    public void setId(Integer id) {this.id = id;}
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(nombre, usuario.nombre) && Objects.equals(apellido, usuario.apellido);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {return  nombre  + " " + apellido + "\n Edad: " + edad  + "\n DNI " + dni +  "\n id:" + id + "\n";}
+}
